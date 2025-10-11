803f349a48593064bf98dc5115ee540c53c0b6d1
RiyadVR <riyadvrasheed.official@gmail.com>
2025-10-11 20:51:59 +0400

    added PublishJSON helper to publish to newly created peril-direct exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -20,10 +20,19 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Connection to the message broker is successful")
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("RabbitMQ connection closed.")
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not establisht the channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(publishCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+	if err != nil {
+		log.Printf("could not publish time: %v", err)
+	}
+
+	fmt.Println("Pause message sent!")
 
 }
---
+++ internal/pubsub/publish_json.go
@@ -0,0 +1,28 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	data, err := json.Marshal(val)
+	if err != nil {
+		log.Fatalf("couldn't marshal the json: %v", err)
+		return err
+	}
+
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        data,
+	})
+
+	if err != nil {
+		log.Fatalf("publishing failed: %v", err)
+	}
+
+	return nil
+}
